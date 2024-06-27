23d0ad5186f0dfc28cb2e589fd8bbf1e4dcddcb5
bii-08 <luuthithanhtam08@gmail.com>
2024-06-27 17:57:45 +1000

    fixed bugs

--- Sources/SwiftSite/main.swift
+++ Sources/SwiftSite/main.swift
@@ -5,5 +5,5 @@ import SplashPublishPlugin
 
 
 // This will generate your website using the built-in Foundation theme:
-try SwiftSite().publish(withTheme: .swiftSite, additionalSteps: [.deploy(using: .gitHub("bii-08/bii-08.github.io", useSSH: false))], plugins: [.splash(withClassPrefix: "")])
+try SwiftSite().publish(withTheme: .swiftSite, deployedUsing: .gitHub("bii-08/bii-08.github.io", useSSH: false), plugins: [.splash(withClassPrefix: "")])
 
