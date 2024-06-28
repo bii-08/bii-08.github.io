4725ca86db50d627464ada93187ef9604e6a539b
Tam Luu <134243117+bii-08@users.noreply.github.com>
2024-06-28 12:14:29 +1000

    Delete file

--- Sources/SwiftSite/main.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Publish
-import Plot
-import SplashPublishPlugin
-
-
-// This will generate your website using the built-in Foundation theme:
-try SwiftSite().publish(withTheme: .swiftSite, additionalSteps: [.deploy(using: .gitHub("bii-08/bii-08.github.io"))], plugins: [.splash(withClassPrefix: "")])
-
